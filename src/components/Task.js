49d554a5a0fe5804c93b235c9ac1f080e2d54e89
Daniel Silva <daniel.silva@direitodeouvir.com.br>
2021-03-31 17:27:50 -0300

    Swipe Excluir Tarefa

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,12 @@
 import React from 'react'
-import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
+import { StyleSheet, Text, View, TouchableWithoutFeedback, TouchableOpacity } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome'
-import commonStyles from '../commonStyles'
+
 import moment from "moment"
 import "moment/locale/pt-br"
+
+import commonStyles from '../commonStyles'
+import Swipeable from 'react-native-gesture-handler/Swipeable'
 
 const getCheckView = (doneAt) => {
     if(doneAt != null){
@@ -22,26 +25,36 @@ export default props => {
     const doneOrNotStyle = props.doneAt != null ? { textDecorationLine: 'line-through' } : {}
     const date = props.doneAt ? props.doneAt : props.estimateAt
     const formattedDate = moment(date).locale('pt-br')
-        .format("ddd, D [de] MMMM [de] YYYY")
+        .format("ddd, D [de] MMMM [de] YYYY");
 
-
-    return(
-        <View style={styles.container}>
-            <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
-                <View style={styles.checkContainer}>
-                    {getCheckView(props.doneAt)}
-                </View>
-            </TouchableWithoutFeedback>
-            <View>
-                <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
-                <Text style={styles.date}>{formattedDate}</Text>
+    const getContent = () => {
+        return(
+            <View style={styles.swipe}>
+                <Icon name="trash" size={20} color="#FFF" style={styles.excludeIcon} />
+                <Text style={styles.excludeText}>Excluir</Text>
             </View>
-        </View>
+        )
+    }
+    return(
+        <Swipeable renderLeftActions={() => getContent()} onSwipeableLeftOpen={() => props.onDelete && props.onDelete(props.id)} >
+            <View style={styles.container}>
+                <TouchableWithoutFeedback onPress={() => props.toggleTask(props.id)}>
+                    <View style={styles.checkContainer}>
+                        {getCheckView(props.doneAt)}
+                    </View>
+                </TouchableWithoutFeedback>
+                <View>
+                    <Text style={[styles.desc, doneOrNotStyle]}>{props.desc}</Text>
+                    <Text style={styles.date}>{formattedDate}</Text>
+                </View>
+            </View>
+        </Swipeable>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
+        backgroundColor: "#FFF",
         flexDirection: "row",
         borderColor: "#AAA",
         borderBottomWidth: 1,
@@ -75,5 +88,20 @@ const styles = StyleSheet.create({
     date: {
         color: commonStyles.colors.subText,
         fontSize: 12
+    },
+    swipe: {
+        flex: 1,
+        backgroundColor: "red",
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+    excludeText: {
+        fontFamily: commonStyles.fontFamily,
+        color: "#FFF",
+        fontSize: 20,
+        margin: 10
+    },
+    excludeIcon: {
+        marginLeft: 10
     }
 })
--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -73,6 +73,11 @@ export default class TaskList extends Component{
         this.setState({tasks, showAddTask: false}, this.filterTasks)
     }
 
+    deleteTask = id => {
+        const tasks = this.state.tasks.filter(task => task.id !== id);
+        this.setState({tasks}, this.filterTasks)
+    }
+
     render(){
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM [de] YYYY')
         return(
@@ -93,7 +98,7 @@ export default class TaskList extends Component{
                     <FlatList 
                         data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask}/>}
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} onDelete={this.deleteTask} />}
                     />
                 </View>
                 <TouchableOpacity activeOpacity={0.5} style={styles.addButton} onPress={() => this.setState({showAddTask: true})}>
